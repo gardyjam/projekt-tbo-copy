ba8e3dc4bf1d9bbb61529236d5632b72a130e529
Monika Gardyjas <01169557@pw.edu.pl>
2026-01-20 20:59:10 +0100

    delete sql

--- project/core/database.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-def get_user_data(user_input):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    query = f"SELECT * FROM users WHERE name = '{user_input}'"
-    cursor.execute(query)
-    return cursor.fetchall()
